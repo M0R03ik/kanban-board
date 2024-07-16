05f98241d59bb6a685b73a690908d56836007dc7
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-07-16 11:54:54 +0300

    Changed the rules for rendering the button. I moved the drawing of the plus sign to css

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,15 +1,12 @@
 import style from './button.module.css'
-import plus from '../../assets/icons/plus.svg'
 
-export const Button = ({ isSubmit, disabled = false }) => {
-  return isSubmit ? (
-    <button disabled={disabled} className={`${style.button} ${style.submit}`}>
-      Submit
-    </button>
-  ) : (
-    <button disabled={disabled} className={style.button}>
-      <img src={plus} alt='' width={14} height={14} />
-      Add card
+export const Button = ({ isSubmit, disabled = false, children }) => {
+  return (
+    <button
+      disabled={disabled}
+      className={isSubmit ? `${style.button} ${style.submit}` : style.button}
+    >
+      {children}
     </button>
   )
 }
