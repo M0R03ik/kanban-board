0d66b1cffc3915cdda71aee7dc3a6aeb7aea66e7
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-07-17 12:42:11 +0300

    Added spread props

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,8 +1,9 @@
 import style from './button.module.css'
 
-export const Button = ({ isSubmit, disabled = false, children }) => {
+export const Button = ({ isSubmit, disabled = false, children, ...props }) => {
   return (
     <button
+      {...props}
       disabled={disabled}
       className={isSubmit ? `${style.button} ${style.submit}` : style.button}
     >
