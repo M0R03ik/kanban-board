9b15a818969737766700b454d8fa75506eb562dd
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-07-22 15:26:36 +0300

    fix: Moved the state for storing the task list to the App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css'
+import { useState } from 'react'
 import { Content } from './Components/Content/Content'
 import { Header } from './Components/Header/Header'
 import { Footer } from './Components/Footer/Footer'
+import { data } from './data'
 
 function App() {
+  const [tasks, setTasks] = useState(data)
   return (
     <div className='App'>
       <Header />
-      <Content />
-      <Footer />
+      <Content tasks={tasks} setTasks={setTasks} />
+      <Footer tasks={tasks} />
     </div>
   )
 }
