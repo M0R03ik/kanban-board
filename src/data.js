aa0daee3ae84f3e96c9acd7ec44cd9c77c178150
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-07-22 15:24:45 +0300

    fix: changed the data structure for the task list

--- src/data.js
+++ src/data.js
@@ -1,147 +1,90 @@
 export const dataMock = [
   {
-    title: 'Backlog',
-    tasks: [
-      {
-        id: '1',
-        name: 'Sprint bugfix',
-        description: 'Fix all the bugs',
-      },
-    ],
+    id: '1',
+    name: 'Sprint bugfix',
+    description: 'Fix all the bugs',
+    status: 'backlog',
   },
   {
-    title: 'Ready',
-    tasks: [
-      {
-        id: '2',
-        name: 'Sprint bugfix',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '345',
-        name: 'Задача 1',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '3464563',
-        name: 'Задача 2',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-      {
-        id: '234234',
-        name: 'Задача 3',
-        description: 'Fix all the bugs',
-      },
-    ],
+    id: '2',
+    name: 'Sprint bugfix',
+    description: 'Fix all the bugs',
+    status: 'backlog',
   },
   {
-    title: 'In Progress',
-    tasks: [
-      {
-        id: '34574568',
-        name: 'Задача в работе 1',
-        description: 'Делай хорошо, а плохо не делай!',
-      },
-      {
-        id: '4564',
-        name: 'Задача в работе 2',
-        description: 'Делай хорошо, а плохо не делай!',
-      },
-      {
-        id: '46345',
-        name: 'Задача в работе 3',
-        description: 'Делай хорошо, а плохо не делай!',
-      },
-      {
-        id: '5667587',
-        name: 'Задача в работе 4',
-        description: 'Делай хорошо, а плохо не делай!',
-      },
-    ],
+    id: '31',
+    name: 'Sprint bugfix',
+    description: 'Fix all the bugs',
+    status: 'backlog',
+  },
+
+  {
+    id: '2',
+    name: 'Sprint bugfix',
+    description: 'Fix all the bugs',
+    status: 'ready',
   },
   {
-    title: 'Finished',
-    tasks: [
-      {
-        id: '234',
-        name: 'Завершенная задача 1',
-        description: 'Всё успешно завершено',
-      },
-      {
-        id: '3467',
-        name: 'Завершенная задача 2',
-        description: 'Всё успешно завершено',
-      },
-      {
-        id: '8678',
-        name: 'Завершенная задача 3',
-        description: 'Всё успешно завершено',
-      },
-    ],
+    id: '345',
+    name: 'Задача 1',
+    description: 'Fix all the bugs',
+    status: 'ready',
+  },
+  {
+    id: '3464563',
+    name: 'Задача 2',
+    description: 'Fix all the bugs',
+    status: 'ready',
+  },
+
+  {
+    id: '234234',
+    name: 'Задача 3',
+    description: 'Fix all the bugs',
+    status: 'ready',
+  },
+
+  {
+    id: '34574568',
+    name: 'Задача в работе 1',
+    description: 'Делай хорошо, а плохо не делай!',
+    status: 'inProgress',
+  },
+  {
+    id: '4564',
+    name: 'Задача в работе 2',
+    description: 'Делай хорошо, а плохо не делай!',
+    status: 'inProgress',
+  },
+  {
+    id: '46345',
+    name: 'Задача в работе 3',
+    description: 'Делай хорошо, а плохо не делай!',
+    status: 'inProgress',
+  },
+  {
+    id: '5667587',
+    name: 'Задача в работе 4',
+    description: 'Делай хорошо, а плохо не делай!',
+    status: 'inProgress',
+  },
+
+  {
+    id: '234',
+    name: 'Завершенная задача 1',
+    description: 'Всё успешно завершено',
+    status: 'finished',
+  },
+  {
+    id: '3467',
+    name: 'Завершенная задача 2',
+    description: 'Всё успешно завершено',
+    status: 'finished',
+  },
+  {
+    id: '8678',
+    name: 'Завершенная задача 3',
+    description: 'Всё успешно завершено',
+    status: 'finished',
   },
 ]
