10bafaf9a1e68da244e090592d98e688e94a4a6a
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-07-23 17:30:54 +0300

    feat: Include localStorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Content } from './Components/Content/Content'
 import { Header } from './Components/Header/Header'
 import { Footer } from './Components/Footer/Footer'
-import { dataMock } from './data'
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
-  const [tasks, setTasks] = useState(dataMock)
+  const initialValue = JSON.parse(localStorage.getItem('tasks')) || []
+  const [tasks, setTasks] = useState(initialValue)
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
+
   return (
     <BrowserRouter>
       <div className='App'>
