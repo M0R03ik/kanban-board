8c8e26e245b91baf0fa76ec663c38c22f12b442b
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-07-24 13:21:27 +0300

    fix: renamed props, deleted unused state

--- src/Components/DropDown/DropDown.jsx
+++ src/Components/DropDown/DropDown.jsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import style from './DropDown.module.css'
 
-export const DropDown = ({ title, tasks, onClick }) => {
-  const [taskName, setTaskName] = useState('Select task')
+export const DropDown = ({ tasksList, type, onClick }) => {
   const [isOpenMenu, setIsOpenMenu] = useState(false)
+
   return (
     <div className={style.wrap}>
       <div className={style.control} onClick={() => setIsOpenMenu(!isOpenMenu)}>
-        {taskName}
+        {'Select task'}
       </div>
       {isOpenMenu ? (
         <div className={style.menu}>
           <ul className={style.list}>
-            {tasks.map(task => {
+            {tasksList.map(task => {
               return (
                 <li
                   key={task.id}
                   className={style.item}
-                  onClick={() => onClick(title, task.id)}
+                  onClick={() => onClick(type, task.id)}
                 >
                   {task.name}
                 </li>
