99df538c7294a92239c273dfd51e8387037ba926
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-07-24 13:36:31 +0300

    feat: added output of the number of active and completed tasks, added a link to github

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,18 +1,31 @@
 import style from './style.module.css'
+import { LIST_TYPES } from '../../config'
 
-export const Footer = props => {
+export const Footer = ({ tasks }) => {
+  const activeTasks = tasks.filter(task => task.status === LIST_TYPES.BACKLOG)
+  const finishedTasks = tasks.filter(
+    task => task.status === LIST_TYPES.FINISHED
+  )
   return (
     <footer className={style.footer}>
       <div className={style.tasks}>
         <p className={style.task}>
-          Active tasks: <span className='active'>&lt;N&gt;</span>
+          Active tasks: <span className='active'>{activeTasks.length}</span>
         </p>
         <p className={style.task}>
-          Finished tasks: <span className='active'>&lt;M&gt;</span>
+          Finished tasks: <span className='active'>{finishedTasks.length}</span>
         </p>
       </div>
       <div className='copyRight'>
-        Kanban board by &lt;NAME&gt;, &lt;YEAR&gt;
+        Kanban board by{' '}
+        <a
+          className={style.link}
+          href='https://github.com/M0R03ik'
+          target='blanc'
+        >
+          Mikhail
+        </a>{' '}
+        &lt;2024&gt;
       </div>
     </footer>
   )
