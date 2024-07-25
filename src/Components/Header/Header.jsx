d80e2a7f2c2a2bfbb6bbe9ef7b7c12459310f3e5
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-07-25 15:46:15 +0300

    feat: Added a user drop-down menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,40 @@
 import avatar from '../../assets/images/user-avatar.svg'
 import arrow from '../../assets/icons/arrow.svg'
 import style from './style.module.css'
+import { useState } from 'react'
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <header className={style.header}>
       <h1 className={style.title}>Awesome Kanban Board</h1>
-      <div className={style.userMenu}>
+      <div className={style.userMenu} onClick={handleClick}>
         <div className={style.avatar}>
           <img src={avatar} alt='Аватар' />
         </div>
-        <button className={style.menuButton}>
+        <div
+          className={
+            isOpen
+              ? `${style.menuButton} ${style.rotate}`
+              : `${style.menuButton}`
+          }
+        >
           <img src={arrow} alt='' width={12} height={8} />
-        </button>
+        </div>
+        {isOpen && (
+          <nav className={style.menu}>
+            <ul className={style.menuList}>
+              <li className={style.menuItem}>Profile</li>
+              <li className={style.menuItem}>Log Out</li>
+            </ul>
+            <div className={style.triangle}></div>
+          </nav>
+        )}
       </div>
     </header>
   )
